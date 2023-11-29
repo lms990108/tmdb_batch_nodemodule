63dd24eae7979d3aae469a0186365ca34ca4f962
lee minseop <lms990108@gmail.com>
2023-11-30 03:50:06 +0900

    Feat: 로그파일 관련해서 index도 수정

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 require("dotenv").config();
 const fetchAndStoreMovieDetails = require("./movieService");
 const fetchAndStoreTVShowDetails = require("./tvshowService");
+const logger = require("./logger");
 
-// 메인 로직을 여기서 구성합니다.
 async function main() {
   for (let i = 0; i < 1000000; i += 1000) {
-    await fetchAndStoreTVShowDetails(i, i + 1000);
-    await fetchAndStoreMovieDetails(i, i + 1000);
+    try {
+      logger.info(`Processing TV shows for range ${i} - ${i + 999}`);
+      await fetchAndStoreTVShowDetails(i, i + 999);
+
+      logger.info(`Processing movies for range ${i} - ${i + 999}`);
+      await fetchAndStoreMovieDetails(i, i + 999);
+    } catch (error) {
+      logger.error(`Error in main loop: ${error.message}`);
+    }
   }
 }
 
-main().catch(console.error);
+main().catch((error) => {
+  logger.error(`Fatal error in main function: ${error.message}`);
+});
